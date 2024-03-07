0a4c62e0fb67e9b2464586421ac25b5fdfb9dec3
Petra Barus <petra.barus@gmail.com>
2024-03-07 08:17:43 +0700

    add export.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,17 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    id("maven-publish")
 }
+
+group = "net.petrabarus.poc.shared.kmmmaven"
+version = "1.1"
+
+// Load local.properties
+val rootLocalProperties = Properties()
+rootLocalProperties.load(rootProject.file("local.properties").inputStream())
 
 kotlin {
     androidTarget {
@@ -24,6 +34,15 @@ kotlin {
     }
 
     sourceSets {
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain.get())
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+        }
 //        commonMain.dependencies {
 //            //put your multiplatform dependencies here
 //        }
@@ -42,5 +61,18 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/petrabarus/poc-android-export-kmm-module-to-maven")
+            credentials {
+                username = rootLocalProperties.getProperty("grp.username")
+                password = rootLocalProperties.getProperty("grp.token")
+            }
+        }
     }
 }
